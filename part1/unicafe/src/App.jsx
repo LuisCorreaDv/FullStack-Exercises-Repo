47311c11f82b66bc9008c8668e2529dc8cafacc5
LuisCorreaDv <dev.luis.correa@gmail.com>
2024-04-22 19:57:39 -0600

    1.7 Unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -27,18 +27,28 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const updateBad = () => {
-    setBad(bad+1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotal(updatedBad + neutral + good);
   }
 
   const updateNeutral = () => {
-    setNeutral(neutral+1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotal(bad + updatedNeutral + good);
   }
 
   const updateGood = () => {
-    setGood(good+1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotal(bad + neutral + updatedGood);
   }
+
+  const positive = (100 * good) / total;
+  const average = (good - bad) / total; 
 
   return (
     <div>
@@ -47,6 +57,10 @@ const App = () => {
       <Button text={"Neutral"} handleClick={updateNeutral}/>
       <Button text={"Good"} handleClick={updateGood}/>
       <Stats good={good} neutral={neutral} bad={bad}/>
+      <p>All {total}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive} %</p>
+      
     </div>
   )
 }
